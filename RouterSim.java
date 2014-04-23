8d39171f248c6c4bf9ab0e41504216f9319836ea
Justin Risch <JustinRisch@gmail.com>
2014-04-23 09:03:27 -0400

    Version 1
    added new features that were missing, completed djikstras via an
    adjacency matrix, and all in all finished most the project.

--- RouterSim.java
+++ RouterSim.java
@@ -13,7 +13,10 @@ public class RouterSim {
 		Double distance; 
 		System.out.println("Welcome to RouterSim.\nPlease use the following commands:\nadd [device name] - Adds a device to the network. It must start with R or C to indicate type of device.");
 		System.out.println("connect [device name] [device name] [distance]");
-		System.out.println("djikstra");
+		System.out.println("reconnect [device name] [device name] [distance]");
+
+		System.out.println("djikstra [from] [to] - finds the shortest route to a device from a device.");
+		System.out.println("routetable - Prints out a Route Table for all devices.");
 		//assign the value of s.nextLine's lower case version. if it says "exit", stop executing. 
 		while((input=s.nextLine().toLowerCase())!="exit"){
 			success=false;
@@ -37,24 +40,43 @@ public class RouterSim {
 				if (numfound==2){
 					success=(Network.get(index1).MakeConnection(Network.get(index2), distance) && Network.get(index2).MakeConnection(Network.get(index1), distance));
 				} 
-
-				//proper syntax to add a device: "add devicename" 
-			} else if (input.startsWith("add") && input.split(" ").length==2){
-				name = input.split(" ")[1].toLowerCase(); 
-
-				//if it starts with an R, add a router. else if it starts with a C, add a client. 
-				if (name.startsWith("r")) {
-					Network.add(new Router(name));
-					Network.get(Network.size()-1).index=Network.size()-1;
-					System.out.println(Network.get(Network.size()-1));
-					success=true;
-				}else if (name.startsWith("c")){
-					Network.add(new Client(name));
-					Network.get(Network.size()-1).index=Network.size()-1;
-					System.out.println(Network.get(Network.size()-1));
-					success=true;
+			} else if (input.startsWith("send") && input.split(" ").length==5){ 
+				String[] temp = input.split(" "); 
+				String route = FindDistance(lookUpDevice(temp[3]), lookUpDevice(temp[4]));
+				if (route.contains("Infinity") || route.contains("Self")){
+					System.out.println("Packets cannot be sent to self or a non-connected device.");
+					success=null;
+				} else {
+					System.out.println(route);
+				device.sendPacket(Integer.parseInt(temp[1]), temp[2], lookUpDevice(temp[3]), lookUpDevice(temp[4])); 
+				}
+			} else if (input.startsWith("routetable")){ 
+				for (device d : Network){
+					System.out.println("------"+d.name+"------");
+					for (device e : Network)
+						System.out.println(FindDistance(d,e));
 				}
 
+				success=null; 
+			} else if (input.startsWith("add") && input.split(" ").length>=2){
+				for (int i =1; i<input.split(" ").length;i++){
+					name = input.split(" ")[i].toLowerCase(); 
+					//if it starts with an R, add a router. else if it starts with a C, add a client. 
+					if (name.startsWith("r")) {
+						Network.add(new Router(name));
+						Network.get(Network.size()-1).index=Network.size()-1;
+						System.out.println(Network.get(Network.size()-1));
+						success=true;
+					}else if (name.startsWith("c")){
+						Network.add(new Client(name));
+						Network.get(Network.size()-1).index=Network.size()-1;
+						System.out.println(Network.get(Network.size()-1));
+						success=true;
+					}else {
+						success=false;
+						break;
+					}
+				}
 				// shows each device, with it's connections listed below it in form of device - distance
 			} else if (input.startsWith("showall")){
 				if (Network.size()>0){
@@ -126,27 +148,32 @@ public class RouterSim {
 			}
 		return temp; 
 	}
-		 
+
 	public static String FindDistance(device start, device target){
+		if (start.equals(target))
+			return "Self-0";
 		int i1 = lookUpIndex(start), i2 = lookUpIndex(target);
-		String path = start.name;
+
+		String[][] path = new String[Network.size()][Network.size()];
+		for(int j=0; j<path.length; j++) 
+			for(int x=0; x<path[1].length; x++) 
+				path[j][x]=lookUpDevice(j).name;
 		Double[][] net = convert(Network);		
-		
-			for(int j=0; j<net.length; j++) {
-				for(int x=0; x<net.length; x++) {
-					//System.out.println("j:"+j + " i:"+i+" x:"+x);
-					double testnum=net[i1][j]+net[j][x];
-					//System.out.println(testnum);
-					if (net[i1][j] > 0 && net[j][x] > 0){
-						if (testnum< net[i1][x] || net[i1][x]<0) {
-							net[i1][x]=testnum;
-							net[x][i1]=testnum;
-							path+=" " +lookUpDevice(x).name;
-						}
+
+		for(int j=0; j<net.length; j++) {
+			for(int x=0; x<net.length; x++) {
+				double testnum=net[i1][j]+net[j][x];
+				if (net[i1][j] > 0 && net[j][x] > 0){
+					if (testnum< net[i1][x] || net[i1][x]<0) {
+						net[i1][x]=testnum;
+						net[x][i1]=testnum;
+						path[i1][x]=path[i1][j]+"-"+net[i1][j]+" " +path[j][x]+"-"+net[j][x];	
+
 					}
 				}
+			}
 		}
-		return path+":"+net[i1][i2];
+		return path[i1][i2]+" "+target.name+":"+net[i1][i2];
 	}
 }
 abstract class device  {
@@ -155,9 +182,9 @@ abstract class device  {
 	public ArrayList<Double> distances = new ArrayList<Double>(0);
 	public ArrayList<device> connections = new ArrayList<device>(0);
 	public boolean MakeConnection(device X, Double Y){ return false;}//to be overloaded. 
-
+	public boolean reconnect(device X, Double Y){return false;}
 	//Simulates the sending of a packet. 
-	public void sendPacket(int PacketSize, String message, device Target){
+	public static void sendPacket(int PacketSize, String message, device source, device Target){
 		byte[] bytes = message.getBytes(), temp = new byte[PacketSize]; 
 		ArrayList<byte[]> packets = new ArrayList<byte[]>();	
 		//packing packets
@@ -186,7 +213,7 @@ abstract class device  {
 		}
 	}
 	public String toString(){
-		String re=name+":"+index+"_connections"; 
+		String re=name+":"+index+"_"; 
 		for (int i = 0; i < connections.size(); i++)
 		{
 			re +="\n"+connections.get(i).name + "-" +  distances.get(i);
@@ -199,6 +226,8 @@ class Client extends device  {
 	public Client() {}
 	public Client(String X) {name = X; }
 	//overloading methods 
+	public boolean reconnect(device X, Double Y){return MakeConnection(X,Y);}
+
 	public boolean MakeConnection(device X, Double Y){
 		//now we remove the previous connection as only one is allowed at a time. 
 		if (connections.size()>0){
@@ -220,6 +249,11 @@ class Router extends device {
 	public boolean MakeConnection(device X, Double Y){
 		distances.add(Y);
 		connections.add(X);
+		return true; 
+	}
+	public boolean reconnect(device X, Double Y){
+		removeConnection(X.name);
+		MakeConnection(X, Y);
 		return true; 
 	}
 	public boolean removeConnection(String name){
